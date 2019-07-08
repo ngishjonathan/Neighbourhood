6a2095bce2b3f6927a3de01a708230dbe0bec0d0
ngishjonathan <balozimwash@gmil.com>
2019-07-08 15:00:54 +0300

    add forms for registration

--- neighborhood/admin.py
+++ neighborhood/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Profile,Business,Neighbourhood,Post
 
-# admin.site.register(Profile)
-# admin.site.register(Neighbourhood)
-# admin.site.register(Business)
-# admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Post)
 
